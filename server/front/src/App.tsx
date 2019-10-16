004a6279a16be28c8f6b2a4408a6f89544c9388d
fosemberg <fosemberg@gmail.com>
2019-10-16 14:27:11 +0300

    main page

--- server/front/src/App.tsx
+++ server/front/src/App.tsx
@@ -1,23 +1,30 @@
 import React from 'react';
-
 import Jumbotron from 'react-bootstrap/Jumbotron'
 import Container from 'react-bootstrap/Container'
+import {Button, Form, FormControl, InputGroup} from "react-bootstrap";
 
-import ButtonsShowcase from './showcases/Buttons'
-import ToastsShowcase from './showcases/Toasts'
+const App: React.FC = () => (
+  <Container className="p-3">
+    <Jumbotron>
+      <h1 className="header">Fosemberg CI</h1>
+    </Jumbotron>
 
-const App: React.FC = () => {
-  return (
-    <Container className="p-3">
-      <Jumbotron>
-        <h1 className="header">Welcome To React-Bootstrap Typescript Example</h1>
-      </Jumbotron>
-      <h2>Buttons</h2>
-      <ButtonsShowcase/>
-      <h2>Toasts</h2>
-      <ToastsShowcase/>
-    </Container>
-  );
-}
+    <Form>
+      <Form.Group controlId="formBasicEmail">
+        <Form.Label>hash commit</Form.Label>
+        <Form.Control type="text" placeholder="hash commit"/>
+      </Form.Group>
+
+      <Form.Group controlId="exampleForm.ControlTextarea1">
+        <Form.Label>command</Form.Label>
+        <Form.Control as="textarea" rows="3"/>
+      </Form.Group>
+
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+  </Container>
+);
 
 export default App;
