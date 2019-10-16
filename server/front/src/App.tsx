c2e8916134d6d953a07b004b4d83e99da7e8de8a
fosemberg <fosemberg@gmail.com>
2019-10-16 15:48:33 +0300

    add router

--- server/front/src/App.tsx
+++ server/front/src/App.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
+import {BrowserRouter, Route, Switch} from "react-router-dom";
 import Jumbotron from 'react-bootstrap/Jumbotron'
 import Container from 'react-bootstrap/Container'
 import MainPage from "./pages/MainPage/MainPage";
+import CINavbar from "./components/CINavbar/CINavbar";
+import BuildPage from "./pages/BuildPage/BuildPage";
 
 const App: React.FC = () => (
-  <Container className="p-3">
-    <Jumbotron>
-      <h1 className="header">Fosemberg CI</h1>
-    </Jumbotron>
-
-    <MainPage/>
-  </Container>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <Container className="p-3">
+      <CINavbar/>
+      <Switch>
+        <Route path='/' exact={true} component={MainPage}/>
+        <Route path="/build" component={BuildPage}/>
+      </Switch>
+    </Container>
+  </BrowserRouter>
 );
 
 export default App;
