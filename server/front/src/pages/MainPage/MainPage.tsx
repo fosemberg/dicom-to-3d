fee0465caf2325faca2efe182c72bc8769131bdf
fosemberg <fosemberg@gmail.com>
2019-10-17 10:24:35 +0300

    clean

--- server/front/src/pages/MainPage/MainPage.tsx
+++ server/front/src/pages/MainPage/MainPage.tsx
@@ -2,12 +2,9 @@ import * as React from 'react';
 import BuildTable from "../../components/BuildTable/BuildTable";
 import BuildForm from "../../components/BuildForm/BuildForm";
 import './MainPage.scss';
-import {Button} from "react-bootstrap";
 
 const MainPage: React.FC = () => (
   <div className="MainPage">
-    <Button variant="success">Success</Button>
-    <Button variant="danger">Danger</Button>
     <BuildForm/>
     <BuildTable/>
   </div>
