aa87dfc03b8e1b4c3108daf29368c3586b65a535
fosemberg <fosemberg@gmail.com>
2019-10-17 16:34:10 +0300

    init server back

---
+++ server/back/src/config.ts
@@ -0,0 +1,21 @@
+import { Response } from 'express';
+import { createMessageObject } from './configUtils';
+
+export const PORT = 3020;
+
+export const PATH_TO_REPOS = 'repos';
+export const PATH_TO_BACKUP = 'backup';
+
+export enum MESSAGE {
+  NO_ROUT = 'Rout not found.',
+  NO_REPOSITORY = "Can't download repository with this url. Maybe we have already got repository with this name or url is not correct",
+  REPOSITORY_DELETED = 'repository successfully deleted',
+  REPOSITORY_CLONED = 'repository successfully cloned',
+}
+
+export const RESPONSE = {
+  NO_ROUT: (res: Response) => () =>
+    res.status(404).json(createMessageObject(MESSAGE.NO_ROUT)),
+  NO_REPOSITORY: (res: Response) => () =>
+    res.status(500).json(createMessageObject(MESSAGE.NO_REPOSITORY)),
+};
---
+++ server/back/src/index.test.ts
@@ -0,0 +1,27 @@
+import {PORT} from "./config";
+
+const axios = require(`axios`);
+
+const buildId = 2;
+const repositoryId = `server-info`;
+const hashCommit = 'master';
+const command = 'node console.js';
+
+const url = `https://github.com/fosemberg/${repositoryId}.git`;
+const host = `http://localhost:${PORT}`;
+
+
+const checkUrl = (url, type = `get`, body = {}) => {
+  const _url = `${host}${url}`;
+  return axios[type](_url, body)
+    .then((response) => {
+      console.log(type, _url);
+      console.log(response.data);
+    })
+    .catch((error) => {
+      console.log(type, _url);
+      console.log(error.response.data);
+    });
+};
+
+checkUrl(`/build/${buildId}/${repositoryId}/${hashCommit}/${command}`)
