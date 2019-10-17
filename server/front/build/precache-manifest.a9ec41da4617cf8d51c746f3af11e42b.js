019f18fc32a721e8a345e57ac1f695e670ba26c8
fosemberg <fosemberg@gmail.com>
2019-10-17 12:57:50 +0300

    good environment

---
+++ server/front/build/precache-manifest.a9ec41da4617cf8d51c746f3af11e42b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f24968b0b3e739856d0eedb51ef65555",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5683561e2052c80af637",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "066cc6761af8f7fd9e18",
+    "url": "/static/css/main.a21c6808.chunk.css"
+  },
+  {
+    "revision": "5683561e2052c80af637",
+    "url": "/static/js/2.ac2ef968.chunk.js"
+  },
+  {
+    "revision": "066cc6761af8f7fd9e18",
+    "url": "/static/js/main.4731560f.chunk.js"
+  },
+  {
+    "revision": "ebd3a683af0fa2dbb56f",
+    "url": "/static/js/runtime-main.7139b951.js"
+  }
+]);
