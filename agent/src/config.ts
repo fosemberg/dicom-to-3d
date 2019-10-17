7de3385f2b00a0721d93f9d74a3ef855cf7a8393
fosemberg <fosemberg@gmail.com>
2019-10-17 15:21:00 +0300

    fix exports

--- agent/src/config.ts
+++ agent/src/config.ts
@@ -6,24 +6,16 @@ export const PORT = 3020;
 const PATH_TO_REPOS = 'repos';
 const PATH_TO_BACKUP = 'backup';
 
-enum MESSAGE {
+export enum MESSAGE {
   NO_ROUT = 'Rout not found.',
   NO_REPOSITORY = "Can't download repository with this url. Maybe we have already got repository with this name or url is not correct",
   REPOSITORY_DELETED = 'repository successfully deleted',
   REPOSITORY_CLONED = 'repository successfully cloned',
 }
 
-const RESPONSE = {
+export const RESPONSE = {
   NO_ROUT: (res: Response) => () =>
     res.status(404).json(createMessageObject(MESSAGE.NO_ROUT)),
   NO_REPOSITORY: (res: Response) => () =>
     res.status(500).json(createMessageObject(MESSAGE.NO_REPOSITORY)),
-};
-
-module.exports = {
-  PATH_TO_REPOS,
-  PATH_TO_BACKUP,
-  MESSAGE,
-  RESPONSE,
-  PORT,
 };
