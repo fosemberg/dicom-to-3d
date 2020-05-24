dc1c07bf59d22c82463d997ca6131428d0868e9d
fosemberg <fosemberg@gmail.com>
2020-05-24 18:13:32 +0300

    add run script for ubuntu

---
+++ ubuntu-install.sh
@@ -0,0 +1,35 @@
+# main
+sudo apt update
+sudo apt upgrade
+sudo apt install git
+# nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+# reload to console
+
+# nodejs
+nvm install node
+npm i -g yarn
+npm i -g pm2
+
+# python
+sudo apt install python3-pip
+
+# clone project
+mkdir git
+cd git
+git clone https://github.com/fosemberg/dicom-to-3d.git
+
+# run serer front
+cd ~/git/dicom-to-3d/server/front
+yarn
+pm2 start npm -- start
+
+# run server back
+cd ~/git/dicom-to-3d/server/back
+yarn
+pm2 start npm -- run dev
+
+# python deps
+pip3 install SimpleITK
+pip3 install vtk
+pip3 install numpy
