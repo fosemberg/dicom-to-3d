3849c027986f225a8ebd5f829f20b3f9ca065e7f
fosemberg <fosemberg@gmail.com>
2020-06-16 22:11:55 +0300

    transliterate while typing

--- server/front/src/components/UploadForm/UploadForm.tsx
+++ server/front/src/components/UploadForm/UploadForm.tsx
@@ -31,6 +31,29 @@ enum SendStatus {
 
 const cnUploadForm = cn('UploadForm');
 
+function transliterate(word: any){
+  let answer = ""
+  let a: any = {};
+
+  a["Ё"]="YO";a["Й"]="I";a["Ц"]="TS";a["У"]="U";a["К"]="K";a["Е"]="E";a["Н"]="N";a["Г"]="G";a["Ш"]="SH";a["Щ"]="SCH";a["З"]="Z";a["Х"]="H";a["Ъ"]="'";
+  a["ё"]="yo";a["й"]="i";a["ц"]="ts";a["у"]="u";a["к"]="k";a["е"]="e";a["н"]="n";a["г"]="g";a["ш"]="sh";a["щ"]="sch";a["з"]="z";a["х"]="h";a["ъ"]="'";
+  a["Ф"]="F";a["Ы"]="I";a["В"]="V";a["А"]="a";a["П"]="P";a["Р"]="R";a["О"]="O";a["Л"]="L";a["Д"]="D";a["Ж"]="ZH";a["Э"]="E";
+  a["ф"]="f";a["ы"]="i";a["в"]="v";a["а"]="a";a["п"]="p";a["р"]="r";a["о"]="o";a["л"]="l";a["д"]="d";a["ж"]="zh";a["э"]="e";
+  a["Я"]="Ya";a["Ч"]="CH";a["С"]="S";a["М"]="M";a["И"]="I";a["Т"]="T";a["Ь"]="'";a["Б"]="B";a["Ю"]="YU";
+  a["я"]="ya";a["ч"]="ch";a["с"]="s";a["м"]="m";a["и"]="i";a["т"]="t";a["ь"]="'";a["б"]="b";a["ю"]="yu";
+
+  for (const i in word){
+    if (word.hasOwnProperty(i)) {
+      if (a[word[i]] === undefined){
+        answer += word[i];
+      } else {
+        answer += a[word[i]];
+      }
+    }
+  }
+  return answer;
+}
+
 const generateStlUrl = (projectName: string, stlModeName: STL_MODE_NAME) => `${SERVER_STATIC_URL}/projects/${projectName}/${stlModeName}.stl`
 
 const UploadForm: React.FC<UploadFormProps> = (
@@ -44,7 +67,9 @@ const UploadForm: React.FC<UploadFormProps> = (
   const [responseMessage, setResponseMessage] = useState<string>('')
 
   const [projectName, setProjectName] = useState<string>('');
-  const onChangeProjectName = (e: React.ChangeEvent<HTMLInputElement>) => setProjectName(e.currentTarget.value);
+  const onChangeProjectName = (e: React.ChangeEvent<HTMLInputElement>) => (
+    setProjectName(transliterate(e.currentTarget.value))
+  )
 
   const [projectNameSended, setProjectNameSended] = useState<string>('');
 
