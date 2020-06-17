f79d8290ed8851df8442eebd368141fd220a3b47
fosemberg <fosemberg@gmail.com>
2020-06-17 22:56:32 +0300

    clean back

--- server/back/config/env.ts
+++ server/back/config/env.ts
@@ -1,3 +1,1 @@
-export const SERVER_WS_PORT = Number(process.env.SERVER_WS_PORT) || 8021;
 export const SERVER_HTTP_PORT = Number(process.env.SERVER_HTTP_PORT) || 3021;
-export const REPOSITORY_URL = process.env.REPOSITORY_URL || 'https://github.com/fosemberg/server-info.git';
