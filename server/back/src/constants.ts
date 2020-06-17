a54d79b128a1fe7e3d9190506c89d8b1d284fcb7
fosemberg <fosemberg@gmail.com>
2020-06-17 22:56:08 +0300

    clean back

--- server/back/src/constants.ts
+++ server/back/src/constants.ts
@@ -1,8 +1,1 @@
-export const DB_DIR = `${__dirname}/../../db`
-export const DB_NAME = 'db.json';
-export const DB_FULL_PATH = `${DB_DIR}/${DB_NAME}`;
 export const STATIC_DIR = `${__dirname}/../public`;
-
-export enum MESSAGE {
-  CONNECTED = 'successfully conected'
-}
